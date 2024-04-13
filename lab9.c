6cff39ce94b1be8a33f740347bd9cef99547e062
Sebastian-G-0 <160250834+Sebastian-G-0@users.noreply.github.com>
2024-04-12 23:20:54 -0400

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,94 +1,137 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// RecordType
 struct RecordType
 {
-	int		id;
-	char	name;
-	int		order; 
+    int identifier;
+    char label;
+    int sequence;
 };
 
-// Fill out this structure
-struct HashType
+struct HashNode
 {
-
+    struct RecordType data;
+    struct HashNode *next;
 };
 
-// Compute the hash function
+struct HashTable
+{
+    struct HashNode *bucket;
+};
+
 int hash(int x)
 {
-
+    return x % 23; 
 }
 
-// parses input file to an integer array
-int parseData(char* inputFileName, struct RecordType** ppData)
+int parseInputFile(char *fileName, struct RecordType **pData)
 {
-	FILE* inFile = fopen(inputFileName, "r");
-	int dataSz = 0;
-	int i, n;
-	char c;
-	struct RecordType *pRecord;
-	*ppData = NULL;
+    FILE *file = fopen(fileName, "r");
+    int i;
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (inFile)
-	{
-		fscanf(inFile, "%d\n", &dataSz);
-		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
-		// Implement parse data block
-		if (*ppData == NULL)
-		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
-		}
-		for (i = 0; i < dataSz; ++i)
-		{
-			pRecord = *ppData + i;
-			fscanf(inFile, "%d ", &n);
-			pRecord->id = n;
-			fscanf(inFile, "%c ", &c);
-			pRecord->name = c;
-			fscanf(inFile, "%d ", &n);
-			pRecord->order = n;
-		}
+    int dataSize;
+    fscanf(file, "%d\n", &dataSize);
 
-		fclose(inFile);
-	}
+    *pData = (struct RecordType *)malloc(sizeof(struct RecordType) * dataSize);
+    if (*pData == NULL)
+    {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	return dataSz;
+    for (i = 0; i < dataSize; ++i)
+    {
+        fscanf(file, "%d %c %d", &(*pData)[i].identifier, &(*pData)[i].label, &(*pData)[i].sequence);
+    }
+
+    fclose(file);
+
+    return dataSize;
 }
 
-// prints the records
-void printRecords(struct RecordType pData[], int dataSz)
+void printRecords(struct RecordType data[], int dataSize)
 {
-	int i;
-	printf("\nRecords:\n");
-	for (i = 0; i < dataSz; ++i)
-	{
-		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
-	}
-	printf("\n\n");
+    int i;
+    printf("Data Items:\n");
+    for (i = 0; i < dataSize; ++i)
+    {
+        printf("\t%d %c %d\n", data[i].identifier, data[i].label, data[i].sequence);
+    }
 }
 
-// display records in the hash structure
-// skip the indices which are free
-// the output will be in the format:
-// index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+void displayRecordsInHash(struct HashTable *hashArray[], int hashSize)
 {
-	int i;
+    int i;
+    for (i = 0; i < hashSize; ++i)
+    {
+        if (hashArray[i] != NULL)
+        {
+            printf("Index %d -> ", i);
+            struct HashNode *current = hashArray[i]->bucket;
+            while (current != NULL)
+            {
+                printf("%d %c %d", current->data.identifier, current->data.label, current->data.sequence);
+                current = current->next;
 
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+                if (current != NULL)
+                    printf("-> ");
+                else
+                    break;
+            }
+            printf("\n");
+        }
+    }
 }
 
 int main(void)
 {
-	struct RecordType *pRecords;
-	int recordSz = 0;
+    struct RecordType *dataItems;
+    int dataSize = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
-	printRecords(pRecords, recordSz);
-	// Your hash implementation
+    dataSize = parseInputFile("input_lab_9.txt", &dataItems);
+
+    printRecords(dataItems, dataSize);
+
+    struct HashTable *hashTable[23] = {NULL}; 
+    int i;
+
+    for (i = 0; i < dataSize; ++i)
+    {
+        int index = hash(dataItems[i].identifier);
+        if (hashTable[index] == NULL)
+        {
+            hashTable[index] = (struct HashTable *)malloc(sizeof(struct HashTable));
+            hashTable[index]->bucket = NULL;
+        }
+        struct HashNode *newNode = (struct HashNode *)malloc(sizeof(struct HashNode));
+        newNode->data = dataItems[i];
+        newNode->next = hashTable[index]->bucket;
+        hashTable[index]->bucket = newNode;
+    }
+
+
+    displayRecordsInHash(hashTable, 23);
+
+    for (i = 0; i < 23; ++i)
+    {
+        if (hashTable[i] != NULL)
+        {
+            struct HashNode *current = hashTable[i]->bucket;
+            while (current != NULL)
+            {
+                struct HashNode *temp = current;
+                current = current->next;
+                free(temp);
+            }
+            free(hashTable[i]);
+        }
+    }
+    free(dataItems);
+
+    return 0;
 }
